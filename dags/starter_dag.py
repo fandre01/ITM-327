3f29acf6e7254cf1137f009e122db48d39317509
fandre01 <fandre@byui.edu>
2026-01-26 10:59:19 -0700

    Update Snowflake table name in starter_dag.py for consistency

--- dags/starter_dag.py
+++ dags/starter_dag.py
@@ -22,7 +22,7 @@ from utils import get_snowflake_connection
 ON_OFF_SNOWFLAKE_LOAD_ENABLED = True  # Set to True to enable Snowflake loading
 SNOWFLAKE_DATABASE = os.getenv("SNOWFLAKE_DATABASE", "SNOWBEARAIR_DB") # Default to SNOWBEARAIR_DB
 SNOWFLAKE_SCHEMA = os.getenv("SNOWFLAKE_SCHEMA", "RAW") # Default to RAW
-SNOWFLAKE_TABLE = "BORED_API_ACTIVITIES" # Table name for Bored API data
+SNOWFLAKE_TABLE = "STARTER_DAG_ANDRE_F" # Table name for Bored API data
 
 @dag(
     dag_id="starter_dag",
